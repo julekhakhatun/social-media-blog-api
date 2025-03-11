dbaca3c506d1e4a1a63f66d0d4bc0991ae409b51
julekha Khatun <khatunj@merrimack.edu>
2025-03-11 15:43:30 +0000

    committed partially 1st.

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,11 @@
 package com.example.repository;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+
+@Repository
 public interface AccountRepository {
+
+
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,28 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.repository.AccountRepository;
+import com.example.entity.Account;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public List<Accounts> getAllAccounts(){
+        return accountRepository.findAll();
+
+    }
+
+    public Optional<Account> getUserById(Long id) {
+        return accountRepository.findById(id);
+    }
+
+    public Account createAccount(Long id) {
+        accountRepository.deleteById(id);
+    }
 }
